53d72c29a1ace5c7a901e7e678433a40897d5da3
DanielGordon-ml <daniel.lh.gordon@gmail.com>
2025-05-15 14:46:48 +0300

    Improve data slicing

--- src/general_forecast/preprocess_data.py
+++ src/general_forecast/preprocess_data.py
@@ -24,7 +24,7 @@ data.index = pd.to_datetime(data['financial_year'].astype(int).astype(str) + dat
 data.fillna({'volume': 0}, inplace=True)
 
 # %%
-data = data[~data['doc_type'].isin(['RE', 'ZY', 'ZF'])]
+data = data[~data['doc_type'].isin(['RE', 'ZY', 'ZF', 'ZH'])]
 data = data[data['fund_code'] != 1410]
 
 
@@ -32,24 +32,43 @@ data = data[data['fund_code'] != 1410]
 data['type'] = 'any'
 
 # %%
+data.loc[data['doc_type'] == 'ZC', 'type'] = 'affilated-other'  # Should be before the salary specification
 data.loc[(data['fund_code'].isin([1401, 1402, 1408, 1409, 1411, 1412, 1413, 1414, 1415, 1416, 1099, 1523])) & (data['doc_type'].isin(['ZC', 'ZW'])) & (data['expenditure_type'] == 3010), 'type'] = 'salary'
-data.loc[(data['fund_code'].isin([1400, 1403, 1405, 1406, 1407, 1423, 1425])) & (~data['doc_type'].isin(['KM', 'KT', 'ZH'])) & (~ data['expenditure_type'].isin([2010, 2045])), 'type'] = 'cor'
-data.loc[data['doc_type'].isin(['KM']), 'type'] = 'market'
+data.loc[(data['doc_type'].isin(['KR', 'KG'])) & (data['law'] == '0300'), 'type'] = 'vehicles'  # should be before cor
+data.loc[(data['doc_type'].isin(['KR', 'KG'])) & (data['law'] == '0302'), 'type'] = 'special-transportation'
+data.loc[(data['doc_type'].isin(['KR', 'KG'])) & (data['law'] == '0706'), 'type'] = 'tariffs'
+data.loc[(data['doc_type'].isin(['KR', 'KG'])) & (data['law'] == '2900'), 'type'] = 'insurance'
+data.loc[(data['doc_type'].isin(['KR', 'KG'])) & (data['law'] == '1316'), 'type'] = 'special-compensation'
+data.loc[(data['law'] == '9800'), 'type'] = 'special-research'
+
+data.loc[(data['fund_code'].isin([1400, 1403, 1405, 1406, 1407, 1423, 1425])), 'type'] = 'cor'
 data.loc[data['doc_type'].isin(['ZD']), 'type'] = 'arnona'
+data.loc[data['expenditure_type'] == '1020', 'type'] = 'electricity'
+data.loc[data['expenditure_type'] == '1030', 'type'] = 'water'
+data.loc[data['doc_type'].isin(['KM']), 'type'] = 'market'
 data.loc[data['doc_type'].isin(['KT']), 'type'] = 'KT'
-data.loc[data['doc_type'].isin(['ZH']), 'type'] = 'ZH'
+data.loc[data['doc_type'].isin(['SA']), 'type'] = 'SA'
 
 
 
 
 # %%
-names = ['salary', 'cor', 'arnona', 'KM', 'KT', 'rest']
+names = ['salary', 'cor', 'arnona', 'KM', 'KT', 'electricity', 'water', 'vehicles', 'overseas-transporation', 'tariffs', 'insurance', 'special-compensation', 'special-research', 'SA', 'rest']
 frames = [
 data[data['type'] == 'salary'],
 data[data['type'] == 'cor'],
 data[data['type'] == 'arnona'],
 data[data['type'] == 'market'],
 data[data['type'] == 'KT'],
+data[data['type'] == 'electricity'],
+data[data['type'] == 'water'],
+data[data['type'] == 'vehicles'],
+data[data['type'] == 'special-transportation'],
+data[data['type'] == 'tariffs'],
+data[data['type'] == 'insurance'],
+data[data['type'] == 'special-compensation'],
+data[data['type'] == 'special-research'],
+data[data['type'] == 'SA'],
 data[data['type'] == 'any']]
 
 # %%
