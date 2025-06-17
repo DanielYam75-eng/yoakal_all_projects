7742f1635a80e99c17236d3308855030163e34d3
YoacalResearcher4 <yoacal.researcher4@gmail.com>
2025-06-17 13:17:15 +0300

    Add uploading file ability

---
+++ src/upload_file/__init__.py
@@ -0,0 +1,24 @@
+from dagshub import get_repo_bucket_client
+import argparse
+
+
+def upload(username, bucketname, filepath, key):
+    boto_client = get_repo_bucket_client(username + "/" + bucketname)
+    boto_client.upload_file(bucketname, key, filepath)
+
+def main():
+    parser = argparse.ArgumentParser(description="upload a file to a DagsHub bucket.")
+    # Hardcoded username and bucketname
+    username = 'yoacal.data.science'
+    bucketname = 'new-repo'
+
+    parser.add_argument("key", type=str, help="Key of the file in the bucket")
+    parser.add_argument("-o", "--output", type=str, help="local path of file to upload")
+
+    args = parser.parse_args()
+
+    upload(username, bucketname, args.filepath, args.key)
+
+if __name__ == "__main__":
+    main()
+
