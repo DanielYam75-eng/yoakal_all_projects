bf91fcaf5e5421a453571d67cf8382389133608a
YoacalResearcher4 <yoacal.researcher4@gmail.com>
2025-06-23 18:27:18 +0300

    fix upload file format
    changing the format of the string that getting uploaded and changing keyname to be argument

--- src/upload_file/__init__.py
+++ src/upload_file/__init__.py
@@ -7,11 +7,10 @@ def upload(username, bucketname, filepath, key):
     boto_client.upload_file(filepath, bucketname, key)
 
 def info_file_string(name,source,creation_date,template):
-    info_file_name = f"name={name}"
     info_file_source = f"source={source}"
     info_file_creation_date = f"creation_date={creation_date}"
     info_file_template = f"template={template}"
-    combined_file_info = f"{info_file_name}^{info_file_source}^{info_file_creation_date}^{info_file_template}"
+    combined_file_info = f"{name}^{info_file_source}^{info_file_creation_date}^{info_file_template}"  # note: no "name="
     return combined_file_info
 
 def main():
@@ -20,17 +19,17 @@ def main():
     username = 'yoacal.data.science'
     bucketname = 'new-repo'
 
+    parser.add_argument("keyname", type=str, help="key name for the uploaded file")  # required positional arg
     parser.add_argument("-i", "--input", type=str, help="local path of file to input")
 
     args = parser.parse_args()
 
-    name = input("Enter name: ")
     source = input("Enter source: ")
     creation_date = input("Enter creation date(format: YYYY-MM-DD): ")
     template = input("Enter template date: ")
 
 
-    info_of_file_as_string = info_file_string(name,source,creation_date,template)
+    info_of_file_as_string = info_file_string(args.keyname,source,creation_date,template)
     upload(username, bucketname, args.input, info_of_file_as_string)
 
 if __name__ == "__main__":
