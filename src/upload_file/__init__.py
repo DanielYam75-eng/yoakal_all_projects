c435faf3da691304683f37deb96614b0f511f73c
YoacalResearcher4 <yoacal.researcher4@gmail.com>
2025-06-25 11:43:40 +0300

    Implement new features about template input
    adding dataframe allowed_templates and doing validations with it on the input template to make sure it is valid

--- src/upload_file/__init__.py
+++ src/upload_file/__init__.py
@@ -6,11 +6,29 @@ import pandas as pd
 from list_files import load_files
 from list_files import get__key_name
 import hashlib
+import numpy as np
 
 ERROR_INVALID_NAME = "Must be 1-128 characters and contain only letters, digits, '-', '_', or '.' and not include '^' or '='."
 ERROR_INVALID_DATE = "Invalid date format. Please enter the date in YYYY-MM-DD format."
 ERROR_INVALID_KEY_NAME = "this key name already in the system. choose diffrent type of key name"
 ERROR_DATA_ALREADY_IN_THE_SYSTEM = "this data already in the system. "
+PROMPT_FOR_TEMPLATE = "(type 'list' to list all allowed templates, prefix your template with the characters 'ad-hoc-' to enter a free form template): "
+ERROR_TEMPLATE_DISALLOWED = "The chosen template is disallowed, pleasechoose another one. You may type 'list' to list allowed templates"
+ERROR_TEMPLATE_NOT_KNOWN = "This template is not known. To use ad-hoc template prefix its name with 'ad-hoc-'"
+
+allowed_templates = pd.DataFrame(
+        [["monim-no-re-1", 'BI', True],
+         ["invoices-to-po-1", 'BW', False],
+         ["po-1", 'BW', False],
+         ["monim-by-posting-date-1", 'BW', True],
+         ["po-foreign-currency-1", 'BW', False],
+         ["po-foreign-currency-2", 'BW', True],
+         ["invoices-to-po-2", 'BW', True],
+         ["invoices-to-po-foreign-currency-1", 'BW', True],
+         ["po-2", 'BW', True],
+         ["ZH-foreign-currency-1", 'BI', True],],
+        columns=['template', 'source', 'allowed'])
+
 
 def upload(username, bucketname, filepath, key):
     boto_client = get_repo_bucket_client(username + "/" + bucketname)
@@ -34,11 +52,31 @@ def is_valid_name(name):
         return False
     return True
 
-def get_valid_input(prompt, validator, error_message):
+def get_valid_input_template(prompt, validator, error_message):
     while True:
-        value = input(prompt)
-        if validator(value):
-            return value
+        user_response = input(prompt)
+        if user_response == "list":
+            print(allowed_templates)
+        elif user_response.startswith('ad-hoc-'):
+            if validator(user_response):
+                return user_response, None
+            else:
+                print(error_message)
+        elif user_response in allowed_templates['template'].values:
+            if np.any(allowed_templates.loc[allowed_templates['template'] == user_response, 'allowed']):
+                set_source = allowed_templates.loc[allowed_templates['template'] == user_response, 'source'].values[0]
+                return user_response , set_source
+            else:
+                print(ERROR_TEMPLATE_DISALLOWED)
+        else:
+            print(ERROR_TEMPLATE_NOT_KNOWN)
+
+
+def get_valid_input_source(prompt, validator, error_message):
+    while True:
+        user_response = input(prompt)
+        if validator(user_response):
+            return user_response
         else:
             print(error_message)
 
@@ -72,7 +110,7 @@ def check_md5_valid(username,bucketname,filepath):
 
 def main():
     parser = argparse.ArgumentParser(description="upload a file to a DagsHub bucket.")
-    # Hardcoded username and bucketname
+
     username = 'yoacal.data.science'
     bucketname = 'new-repo'
 
@@ -94,10 +132,15 @@ def main():
         print(ERROR_INVALID_KEY_NAME)
         return
 
-    source = get_valid_input("Enter source: ", is_valid_name, f"Invalid source.{ERROR_INVALID_NAME}")
-    creation_date = get_valid_date("Enter creation date (format: YYYY-MM-DD): ",ERROR_INVALID_DATE)
-    template = get_valid_input("Enter template: ", is_valid_name, f"Invalid template.{ERROR_INVALID_NAME}")
+    template , set_source = get_valid_input_template("Enter template: " + PROMPT_FOR_TEMPLATE, is_valid_name, f"Invalid template.{ERROR_INVALID_NAME}")
 
+    if set_source is not None:
+        source = set_source
+    else:
+        source = get_valid_input_source("Enter source: ", is_valid_name, f"Invalid source.{ERROR_INVALID_NAME}")
+
+    creation_date = get_valid_date("Enter creation date (format: YYYY-MM-DD): ",ERROR_INVALID_DATE)
+    
 
     info_of_file_as_string = info_file_string(args.keyname,source,creation_date,template)
     upload(username, bucketname, args.input, info_of_file_as_string)
