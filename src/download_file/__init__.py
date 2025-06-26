43c6568c68476037e813c1d66f5839def7dcac06
YoacalResearcher2 <yoacal.researcher2@gmail.com>
2025-06-26 12:12:56 +0300

    Move code from main to download function
    doing it so we could call download function without any additonal argumants

--- src/download_file/__init__.py
+++ src/download_file/__init__.py
@@ -4,7 +4,16 @@ from upload_file import info_file_string
 import argparse
 
 
-def download(username, bucketname, filepath, key):
+def download(username, bucketname, filepath, key_name):
+    existing_files = load_files(username, bucketname)
+    if key_name not in existing_files.index:
+        print(f"Key '{key_name}' not found in the bucket '{bucketname}'.")
+        return
+
+    key_row = existing_files.loc[key_name]
+    key = info_file_string(key_name, key_row['Source'], key_row['Creation Date'], key_row['Template'])
+
+    
     boto_client = get_repo_bucket_client(username + "/" + bucketname)
     boto_client.download_file(bucketname, key, filepath)
 
@@ -17,17 +26,11 @@ def main():
     parser.add_argument("key", type=str, help="Key of the file in the bucket")
     parser.add_argument("-o", "--output", type=str, help="Local path to save the downloaded file")
 
+    download(username, bucketname, args.output, args.key)
+
     args = parser.parse_args()
 
-    existing_files = load_files(username, bucketname)
-    if args.key not in existing_files.index:
-        print(f"Key '{args.key}' not found in the bucket '{bucketname}'.")
-        return
-
-    key_row = existing_files.loc[args.key]
-    key = info_file_string(args.key, key_row['Source'], key_row['Creation Date'], key_row['Template'])
-
-    download(username, bucketname, args.output, key)
+    
 
 if __name__ == "__main__":
     main()
