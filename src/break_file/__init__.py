94ccc376d1dc519e625d55d3725e7a128ddc6a0e
YoacalResearcher2 <yoacal.researcher2@gmail.com>
2025-07-22 11:13:49 +0300

    Add break/unbreak functionality
    This is the bedrock over which the functionality of removing/mutating templates will
    be established. You may mutate only broken files.

---
+++ src/break_file/__init__.py
@@ -0,0 +1,107 @@
+from dagshub import get_repo_bucket_client
+import argparse
+import pandas as pd
+from list_files import load_files
+import config_file.constants as const
+from upload_file import info_file_string
+from download_file import download
+from upload_file import upload
+import os
+import hashlib
+
+
+def md5_is_valid(username,bucketname,filepath, keyname):
+    with open(filepath, "rb") as f:
+        file_bytes = f.read()
+    hash_md5_specific_file = hashlib.md5(file_bytes).hexdigest().strip('"')
+    boto_client = get_repo_bucket_client(username + "/" + bucketname)
+    response = boto_client.list_objects_v2(Bucket=bucketname)
+
+    etag_specific_data = None
+    for Contents in response.get('Contents'):
+        if Contents.get('Key').startswith(keyname):
+            etag_specific_data = Contents.get('ETag').strip('"')    
+    if etag_specific_data is None:
+        raise ValueError(f"No matching key found for '{keyname}' in bucket '{bucketname}'.")
+    
+    return hash_md5_specific_file == etag_specific_data
+
+
+def rename_object(username, bucketname, key_name, new_key):
+    retries = 3
+    path_for_temp_file = '.tmp-rename'
+    for _ in range(retries):
+        download(username, bucketname, path_for_temp_file, key_name)
+        if md5_is_valid(username, bucketname, path_for_temp_file):
+            break
+    else:
+        raise ConnectionError(const.ERROR_DOWNLOAD_FAILED)
+    upload(username, bucketname, path_for_temp_file, new_key)
+    os.remove(path_for_temp_file)
+
+
+def reverse_df_to_key_default(keyname: str, list_of_files: pd.DataFrame) -> str:
+    df_key = list_of_files.loc[keyname]
+    key_string_format = info_file_string(keyname, df_key["Source"], df_key["Creation Date"], df_key["Template"])
+    return key_string_format
+ 
+
+def make_new_key(files_in_data,keyname,break_flag):   
+    df_key_to_break = files_in_data[files_in_data.index == keyname]
+    if break_flag == True:
+        new_template_key = df_key_to_break["Template"][0] + "?"
+    else:
+        new_template_key = df_key_to_break["Template"][0][:-1]
+    new_key = info_file_string(df_key_to_break.index[0],df_key_to_break["Source"][0],df_key_to_break["Creation Date"][0],new_template_key)
+    return new_key
+
+
+def break_key(username,bucketname,keyname):
+    files_in_data = load_files(username,bucketname)
+    if keyname not in files_in_data.index:
+        print(const.ERROR_DATA_NOT_FOUND)
+        return
+    else:
+        template = files_in_data.loc[keyname, "Template"]
+        new_key = info_file_string(keyname=keyname,
+                                   source=files_in_data.loc[keyname, "Source"],
+                                   creation_date=files_in_data[keyname, "Creation Date"],
+                                   template=(template + "?") if template[-1] != "?" else template)
+        rename_object(username,bucketname,keyname,new_key)
+
+def unbreak_key(username,bucketname,keyname):
+    files_in_data = load_files(username,bucketname)
+    if keyname not in files_in_data.index:
+        print(const.ERROR_DATA_NOT_FOUND)
+        return
+    else:
+        template = files_in_data.loc[keyname, "Template"]
+        new_key = info_file_string(keyname=keyname,
+                                   source=files_in_data.loc[keyname, "Source"],
+                                   creation_date=files_in_data[keyname, "Creation Date"],
+                                   template=template if template[-1] != "?" else template[:-1])
+        rename_object(username,bucketname,keyname,new_key)
+
+
+def main_break():
+    parser = argparse.ArgumentParser(description="enter key to break file")
+
+    username = 'yoacal.data.science' 
+    bucketname = 'exp-repo'
+    parser.add_argument("keyname", type=str, help="key name for the file to break") 
+    args = parser.parse_args()
+    break_key(username,bucketname,args.keyname)
+
+
+def main_unbreak():
+    parser = argparse.ArgumentParser(description="enter key to unbreak file")
+
+    username = 'yoacal.data.science' 
+    bucketname = 'exp-repo'
+    parser.add_argument("keyname", type=str, help="key name for the file to unbreak") 
+    args = parser.parse_args()
+    unbreak_key(username,bucketname,args.keyname)
+
+    
+if __name__ == "__main__":
+    main_break()
