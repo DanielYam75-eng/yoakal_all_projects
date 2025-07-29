31aaf6a52abb1ed1082187ef911cb847dad59f60
Yoacal Researcher2 <yoacal.researcher2@gmail.com>
2025-07-29 17:17:48 +0300

    Add a error handler to connection errors

--- src/get_client/__init__.py
+++ src/get_client/__init__.py
@@ -1,5 +1,12 @@
 from dagshub import get_repo_bucket_client
+from httpx import ConnectError
+import sys
 
 username = 'yoacal.data.science'
 bucketname = 'exp-repo'
-boto_client = get_repo_bucket_client(username + "/" + bucketname)
+try:
+    boto_client = get_repo_bucket_client(username + "/" + bucketname)
+except ConnectError:
+    print("Could not connect to DagsHub. Please check your internet connection or DagsHub status.", file=sys.stderr)
+    sys.exit(0)
+
