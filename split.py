7673d4366f5a502426db8ee7baa77fc68d07e24f
Israel Jacobowich <yoacal.research@gmail.com>
2025-11-17 11:20:05 +0200

    notebooks Transition to well documented, easy to use, jupyter notebooks.

--- split.py
+++ split.py
@@ -1,9 +1,9 @@
 import pandas as pd
 
 
-PATH = "NN-data-5"
-VARS = ["article", "coin"]
+PATH = "../data/deep learning project data/EMFs"
+VARS = ["coin"]
 
 
 for names, data in pd.read_csv(PATH + ".csv").groupby(VARS):
-    data.drop(columns = VARS).to_csv(PATH + "-" + "-".join(map(str, names)) + ".csv")
+    data.drop(columns = VARS).to_csv(PATH + "-" + "-".join(map(str, names)) + ".csv", index = False)
